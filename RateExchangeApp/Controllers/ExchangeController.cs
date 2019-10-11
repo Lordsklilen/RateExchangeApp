eebc5b63248068a5552f11429fa74f8597274963
Michał Dela <dela_m@interia.pl>
2019-10-11 19:23:25 +0200

    Controller update

--- RateExchangeApp/Controllers/ExchangeController.cs
+++ RateExchangeApp/Controllers/ExchangeController.cs
@@ -10,9 +10,24 @@ namespace RateExchangeApp.Controllers
     public class ExchangeController : ApiController
     {
         [HttpGet]
+        [Route("api/Ping")]
         public string GetPing()
         {
             return "Ping";
+        }
+
+        [HttpGet]
+        [Route("api/Exchange/all")]
+        public string GetExchangeTable()
+        {
+
+            return "All known values From NBP db";
+        }
+        //Route: http://localhost:53470/api/Exchange?value=1&currencyFrom=PLN&currencyTo=EUR
+        [HttpGet]
+        public string GetExchangeValue(double value, string currencyFrom,string currencyTo)
+        {
+            return $"Converting currency from {value} {currencyFrom} to {currencyTo}";
         }
     }
 }
